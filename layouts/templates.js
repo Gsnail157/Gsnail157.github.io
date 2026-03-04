1a0217f7f2a0724fa80870ee8be37e9f0676f0b1
Gsnail157 <GaryWG.0120@gmail.com>
2026-03-04 17:27:15 -0500

    added favicon, icons, and content to about/

--- layouts/templates.js
+++ layouts/templates.js
@@ -27,6 +27,15 @@ class SiteHead extends HTMLElement {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${title}</title>
       <link rel="stylesheet" href="/styles/main.css">
+      <!-- Standard favicon -->
+      <link rel="icon" type="image/x-icon" href="/public/favicon_io/favicon.ico">
+      
+      <!-- PNG for modern browsers -->
+      <link rel="icon" type="image/png" sizes="32x32" href="/public/favicon_io/favicon-32x32.png">
+      <link rel="icon" type="image/png" sizes="16x16" href="/public/favicon_io/-16x16.png">
+      
+      <!-- Apple devices -->
+      <link rel="apple-touch-icon" sizes="180x180" href="/public/favicon_io/apple-touch-icon.png">
     `;
   }
 }
