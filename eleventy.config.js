db49a2973bc9ecf811266b402860b644b2b688a6
Gary <GaryWG.0120@gmail.com>
2026-03-20 01:50:17 -0400

    css changes for blog. Eleventy implementation for gh pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,18 @@
-module.exports = function(eleventyConfig) {
- eleventyConfig.addPassthroughCopy("layouts/templates.js");
- eleventyConfig.addPassthroughCopy("styles/main.css");
- eleventyConfig.addPassthroughCopy("books/books.json");
- eleventyConfig.addPassthroughCopy("books/books.js");
- eleventyConfig.addPassthroughCopy("public");
-};
+const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
 
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+    eleventyConfig.addPassthroughCopy("layouts/templates.js");
+    eleventyConfig.addPassthroughCopy("styles/main.css");
+    eleventyConfig.addPassthroughCopy("books/books.json");
+    eleventyConfig.addPassthroughCopy("books/books.js");
+    eleventyConfig.addPassthroughCopy("public");
+
+  return {
+    // Replace "repository-name" with your actual GitHub repository name
+    pathPrefix: "/",
+    dir: {
+      output: "_site" // Optional: Change output from default '_site' to 'docs'
+    }
+  };
+};
